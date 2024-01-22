a15e9338d6eec209ea36b669a6c00d629c2770b3
Am-Parito <137996707+Am-Parito@users.noreply.github.com>
2024-01-22 09:58:23 +0100

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,22 @@
+package myTry;
+
+public class Rectangle {
+    int x;
+    int y;
+    int width;
+    int height;
+    
+    public Rectangle () {
+        this.x = 0;
+        this.y = 0;
+        this.width = 0;
+        this.height = 0;
+    }
+    
+    public Rectangle (int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
